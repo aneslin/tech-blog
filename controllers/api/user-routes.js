e3855ac9713946415407bac04d0985ce3d78f50b
Aaron Neslin <aneslin125@gmail.com>
2022-03-12 14:22:41 -0500

    added login/logout routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -30,5 +30,45 @@ router.post('/', (req, res) => {
     });
 })
 
+//login route
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: { email: req.body.email },
+  })
+    .then((dbUserData) => {
+      console.log("db-user-datea ====>" , dbUserData)
+      if (!dbUserData) {
+        res.status(400).json({ message: "no user with that email address" });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: " incorrect password" });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.email = dbUserData.email;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: "you are now logged in" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//logout route
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 
 module.exports = router;
