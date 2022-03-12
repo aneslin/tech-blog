772e0f4b671cf2f387268ad70e5e02427f100709
Aaron Neslin <aneslin125@gmail.com>
2022-03-12 14:53:32 -0500

    added working homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,6 @@ const router = require('express').Router()
 const apiRoutes = require('./api/')
 router.use('/api', apiRoutes)
 
+const homepageRoute = require('./home-route')
+router.use("/", homepageRoute)
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const sequelize = require('./config/connection')
 const routes = require('./controllers')
-//const exphbs = require ('express-handlebars')
-//const hbs = exphbs.create( { helpers } )
+const exphbs = require ('express-handlebars')
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create( { helpers } )
 const session = require ('express-session')
-const app = express();
 const path = require('path')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
-
+const app = express();
 const sess = {
   secret: 'super secret secret',
   cookie: {},
@@ -20,8 +20,8 @@ const sess = {
   })
 }
 app.use(session(sess))
-//app.engine('handlebars', hbs.engine)
-//app.set('view engine', 'handlebars')
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 const PORT =  3001;
 
