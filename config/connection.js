da78cfb432fc6144aeddb330c5e6dbddf5372ec8
Aaron Neslin <aneslin125@gmail.com>
2022-03-21 18:15:01 -0400

    change if or format

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
+
+const sequelize = process.env.JAWSDB_URL
+  ? sequelize = new Sequelize(process.env.JAWSDB_URL)
+  : sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
@@ -16,5 +15,5 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
     }
   );
-}
+
 module.exports = sequelize;
