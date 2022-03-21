4ad24c40c83acd21c170a79813c3b9328eab3ace
Aaron Neslin <aneslin125@gmail.com>
2022-03-21 11:36:29 -0400

    log in log out sign up

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,20 +15,28 @@ router.get("/:id", (req, res) => {
       id: req.params.id,
     },
     attributes: { exclude: ["password"] },
-  }).then((userData) => res.json(userData))
+  }).then((userData) => res.json(userData));
 });
 
 //new user
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        password:req.body.password
-    }).then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json({ user: dbUserData, message: "you are now logged in" });
+      });
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
-})
+});
 
 //login route
 router.post("/login", (req, res) => {
@@ -36,11 +44,11 @@ router.post("/login", (req, res) => {
     where: { username: req.body.username },
   })
     .then((dbUserData) => {
-      
       if (!dbUserData) {
         res.status(400).json({ message: "no user with that user name" });
         return;
-      }console.log(req.body)
+      }
+      console.log(req.body);
       const validPassword = dbUserData.checkPassword(req.body.password);
       if (!validPassword) {
         res.status(400).json({ message: " incorrect password" });
@@ -69,6 +77,5 @@ router.post("/logout", (req, res) => {
     res.status(404).end();
   }
 });
-
 
 module.exports = router;
