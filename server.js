1d82bf324b7afbef5b5808828686f487589ee723
Aaron Neslin <aneslin125@gmail.com>
2022-03-21 19:28:13 -0400

    -=-

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const sequelize = require('./config/connection')
+
 const routes = require('./controllers')
 const exphbs = require ('express-handlebars')
 const helpers = require('./utils/helpers')
 const hbs = exphbs.create( { helpers } )
 const session = require ('express-session')
 const path = require('path')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const app = express();
-const PORT =  process.env.PORT || 3001
+const PORT =  process.env.PORT || 3001;
+const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
 const sess = {
   secret: 'super secret secret',
   cookie: {maxAge: 360000},
@@ -24,7 +26,7 @@ app.use(session(sess))
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-;
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
