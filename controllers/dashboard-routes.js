6b96eee8d3885d13b34913f80f965d80e09cc376
Aaron Neslin <aneslin125@gmail.com>
2022-04-09 18:15:07 -0400

    will redirect to log in if attepts to log into dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const {Post, User} = require('../models')
 const withAuth = require("../utils/auth")
 
-router.get('/', (req, res) => {
+router.get('/', withAuth,(req, res) => {
     Post.findAll({
         where:{
             user_id: req.session.user_id
